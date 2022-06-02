a59b8a36e72628621686d1ed13f52f661c7577da
ChirujanoCodding <trdfabris@gmail.com>
2022-06-02 20:26:56 -0300

    typed name of events

--- structure/Events.ts
+++ structure/Events.ts
@@ -1,9 +1,9 @@
+import { ClientEvents } from "discord.js";
 import { Pomodoro } from "./Client";
-
 export abstract class Event {
 	constructor(
 		public client: Pomodoro,
-		public name: string,
+		public name: keyof ClientEvents ,
 		public once: boolean = false
 	) {}
 	public abstract run(...args: any[]): any;
